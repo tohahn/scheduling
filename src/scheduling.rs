fa3ffb4f2c2b91f455e2e2167f34fc055a60a7a7
Tobias Hahn <morowinger@gmail.com>
2017-11-01 00:31:56 +0100

    erste übung

---
+++ src/scheduling.rs
@@ -0,0 +1,116 @@
+#![feature(vec_remove_item)]
+use std::env;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use std::cmp::Ordering;
+
+struct Job {
+	name: String,
+	p: i32,
+	d: i32
+}
+
+impl PartialEq for Job {
+	fn eq(&self, other: &Job) -> bool {
+		self.name == other.name && self.p == other.p && self.d == other.d
+	}
+}
+
+struct Schedule<> {
+	jobs: Vec<Job>,
+	cost: f32
+}
+
+fn read_job_list(filename: &String) -> Vec<Job> {
+	let path = Path::new(filename);
+
+	let mut file = match File::open(&path) {
+		Err(why) => panic!("Couldn't open {}: {}", path.display(), why.description()),
+		Ok(file) => file,
+	};
+
+	let mut s = String::new();
+	match file.read_to_string(&mut s) {
+		Err(why) => panic!("Couldn't read {}: {}", path.display(), why.description()),
+		Ok(_) => (),
+	};
+	
+	let mut jobs = vec![];
+	for line in s.split_whitespace() {
+		let tokens: Vec<&str> = line.split('/').collect();
+		let name = tokens[0].to_string();
+		let p = tokens[1].parse::<i32>().unwrap();
+		let d = tokens[2].parse::<i32>().unwrap();
+		let job = Job { name, p , d };
+		jobs.push(job);
+	}
+
+	return jobs;
+}
+
+fn edf_scheduler(mut jobs: Vec<Job>) -> Schedule {
+	jobs.sort_by(|a, b| a.d.cmp(&b.d));
+	let mut max = <i32>::min_value();
+	let mut time = 0;
+	for j in &jobs {
+		time += j.p;
+		if max < - (j.d - time) {
+			max = j.d - time;
+		}
+	}
+	return Schedule{ jobs, cost: max as f32 };
+}
+
+fn lawler_cost(time: &i32, deadline: &i32) -> f32 {
+	let cost = (time - deadline/2) as f32;
+	if cost < 0. {
+		return 0.;
+	} else {
+		return cost.sqrt();
+	}
+}
+
+fn lawler_get_next(jobs: &Vec<Job>, time: &i32) -> Job {
+	let j = jobs.into_iter().max_by(|a, b| lawler_cost(time, &a.d).partial_cmp(&lawler_cost(time, &b.d)).unwrap_or(Ordering::Equal)).unwrap();
+	return Job{name: j.name.clone(), p: j.p, d: j.d};
+}
+
+fn lawler_scheduler(mut jobs: Vec<Job>) -> Schedule {
+	let mut n_jobs = vec![];
+	let mut time = (&jobs).into_iter().fold(0, |sum, x| sum + x.p).clone();
+	let mut max = 0.;
+	while time > 0 {
+		let j = lawler_get_next(&jobs, &time);
+		if max < lawler_cost(&time, &j.d) {
+			max = lawler_cost(&time, &j.d);
+		}
+		time -= j.p;
+		jobs.remove_item(&j);
+		n_jobs.push(j);
+	}
+	return Schedule{ jobs: n_jobs, cost: max as f32};
+}
+
+fn print_schedule(schedule: Schedule) {
+	println!("The jobs are scheduled in the following order:");
+	for j in &schedule.jobs {
+		print!("{},", j.name);
+	}
+	println!();
+	println!("The total cost is: {}", schedule.cost);
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[2];
+    let scheduler = &args[1];
+
+    let joblist = read_job_list(filename);
+    let schedule = match scheduler.as_str() {
+    	"edf" => edf_scheduler(joblist),
+	_ => lawler_scheduler(joblist),
+    };
+    print_schedule(schedule);
+}
